2d4d9a13113e674a12e5f34346e292764e34f7d2
serversider <146564375+serversider@users.noreply.github.com>
2023-10-05 14:39:09 +0300

    Update source_rcon.class.php
    Fwrite and fread redone it

--- source_rcon.class.php
+++ source_rcon.class.php
@@ -53,7 +53,7 @@ class RCon {
 
 	// Prefix the packet size
 	$data = pack("V",strlen($data)).$data;
-	$handle = fopen("C:/source/test.txt", "r+b");
+	$handle = fopen("C:/source/test.txt", "wb");
 	// Send packet
 	fwrite($handle,$data,strlen($data));
 
@@ -65,7 +65,7 @@ class RCon {
 	//Declare the return array
 	$retarray = array();
 	//Fetch the packet size
-	$handle = fopen("C:/source/test.txt", "r+b");
+	$handle = fopen("C:/source/test.txt", "rb");
 	
 	while ($size = @fread($handle,4)) {
 	    $size = unpack('V1Size',$size);
